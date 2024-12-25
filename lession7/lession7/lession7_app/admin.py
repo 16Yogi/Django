8590ff45df53088d8e01c7e42eff8e49108d8be6
Yogendra Meravi <85734470+16Yogi@users.noreply.github.com>
2024-12-25 18:47:30 +0530

    commit
    commit

--- lession7/lession7/lession7_app/admin.py
+++ lession7/lession7/lession7_app/admin.py
@@ -1,4 +1,23 @@
 from django.contrib import admin
-from .models import lessions
+from .models import lessions,lessionReview,Store,lessionCertificate
 # Register the Lessions model
-admin.site.register(lessions)
+class lessionReviewAdmin(admin.TabularInline):
+    model = lessionReview
+    extra = 2
+
+class lessionVerietyAdmin(admin.ModelAdmin):
+    list_display = ('name','type','date_added')
+    inlines = [lessionReviewAdmin]
+
+class storeAdmin(admin.ModelAdmin):
+    list_display = ('name','location')
+    filter_horizontal = ('lession_list',)
+
+class lessionCertificateAdmin(admin.ModelAdmin):
+    list_display = ('lession','certificate_number')
+
+admin.site.register(lessions,lessionVerietyAdmin)
+admin.site.register(Store,storeAdmin)
+admin.site.register(lessionCertificate,lessionCertificateAdmin)
+
+# admin.site.register(lessions)
--- lession7/lession7/lession7_app/models.py
+++ lession7/lession7/lession7_app/models.py
@@ -19,3 +19,32 @@ class lessions(models.Model):
 
     def __str__(self):
         return self.name
+
+# one to many
+class lessionReview(models.Model):
+    lession = models.ForeignKey(lessions,on_delete=models.CASCADE,related_name='reviews')
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.user.username} review for {self.lession.name}'
+
+# many to many
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    lession_list = models.ManyToManyField(lessions,related_name='lessionos')
+
+    def __str__(self):
+        return self.name 
+
+# one to one
+class lessionCertificate(models.Model):
+    lession = models.OneToOneField(lessions,on_delete=models.CASCADE,related_name='certificate')
+    certificate_number = models.CharField(max_length=100)
+    issued_date = models.DateTimeField(default=timezone.now)
+    valid_untill = models.DateTimeField()
+
+    def __str__(self):
+        return f'Certificate for {self.name.lession}'
